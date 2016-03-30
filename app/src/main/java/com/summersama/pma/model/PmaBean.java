f0aa6c11ad5c2efc63c0d5e72fd5dc4306b6c9b1
LoveLoliii <woshigegefengleng@live.com>
2016-03-30 12:13:00 +0800

    add pmabean

---
+++ app/src/main/java/com/summersama/pma/model/PmaBean.java
@@ -0,0 +1,49 @@
+package com.summersama.pma.model;
+
+/**
+ * Created by woshi on 2016/3/28.
+ */
+public class PmaBean {
+    private int pmarId;
+    private String userName;
+    private String password;
+
+    public int getPmarId() {
+        return pmarId;
+    }
+
+    public void setPmarId(int pmarId) {
+        this.pmarId = pmarId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public PmaBean(){
+
+    }
+    public PmaBean(int pmarId,String userName,String password){
+        super();
+        this.pmarId=pmarId;
+        this.userName=userName;
+        this.password =password;}
+        @Override
+                public String toString(){
+                        return "PmaBeam [pmaId="+pmarId+",userName="+userName+"," +
+                                "password="+password+"]";
+
+    }
+}
